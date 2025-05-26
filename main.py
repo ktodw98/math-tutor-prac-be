30f605360c4ddab52000a0fca78bf5d833be9832
ktodw98 <infuilamust17@gmail.com>
2025-05-27 00:44:02 +0900

    cors issue

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # origins에 명시된 출처에서의 요청을 허용
+    allow_credentials=True,  # 쿠키를 포함한 요청을 허용할지 여부
+    allow_methods=["*"],  # 모든 HTTP 메소드 허용 (GET, POST, PUT, DELETE 등)
+    allow_headers=["*"],  # 모든 HTTP 헤더 허용
+)
 @app.get("/")
 #async def root():
 def root():
